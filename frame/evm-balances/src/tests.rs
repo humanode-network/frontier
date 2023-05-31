9c5a674b9f57e4452b5c7383818d26c7b4ca2f35
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-05-31 14:22:16 +0200

    Add issuance_after_tip test

--- frame/evm-balances/src/tests.rs
+++ frame/evm-balances/src/tests.rs
@@ -1,6 +1,9 @@
 //! Unit tests.
 
+use frame_support::{assert_noop, assert_ok};
+use pallet_evm::{FeeCalculator, Runner};
 use sp_core::{H160, U256};
+use sp_runtime::traits::UniqueSaturatedInto;
 use sp_std::str::FromStr;
 
 use crate::{mock::*, *};
@@ -50,5 +53,39 @@ fn fee_deduction() {
 		// Refund fees as 5 units
 		<<Test as pallet_evm::Config>::OnChargeTransaction as pallet_evm::OnChargeEVMTransaction<Test>>::correct_and_deposit_fee(&charlie, U256::from(5), U256::from(5), imbalance);
 		assert_eq!(EvmBalances::free_balance(&charlie), 95);
+	});
+}
+
+#[test]
+fn issuance_after_tip() {
+	new_test_ext().execute_with(|| {
+		let before_tip = <Test as pallet_evm::Config>::Currency::total_issuance();
+
+		// Set block number to enable events.
+		System::set_block_number(1);
+
+		assert_ok!(<Test as pallet_evm::Config>::Runner::call(
+			alice(),
+			bob(),
+			Vec::new(),
+			U256::from(1),
+			1000000,
+			Some(U256::from(2_000_000_000)),
+			None,
+			None,
+			Vec::new(),
+			true,
+			true,
+			<Test as pallet_evm::Config>::config(),
+		));
+
+		// Only base fee is burned
+		let base_fee: u64 = <Test as pallet_evm::Config>::FeeCalculator::min_gas_price()
+			.0
+			.unique_saturated_into();
+
+		let after_tip = <Test as pallet_evm::Config>::Currency::total_issuance();
+
+		assert_eq!(after_tip, (before_tip - (base_fee * 21_000)));
 	});
 }
