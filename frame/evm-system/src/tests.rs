792f40c6046b8205d14fb7c2ff249a3d1f0108c5
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-06-16 09:57:51 +0300

    Return success for try_mutate_exists_account_not_created test

--- frame/evm-system/src/tests.rs
+++ frame/evm-system/src/tests.rs
@@ -241,6 +241,32 @@ fn try_mutate_exists_account_removed() {
 	});
 }
 
+/// This test verifies that try_mutate_exists works as expected in case data wasn't providing
+/// and returned data is `None`. As a result, the account hasn't been created.
+#[test]
+fn try_mutate_exists_account_not_created() {
+	new_test_ext().execute_with(|| {
+		// Prepare test data.
+		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
+
+		// Check test preconditions.
+		assert!(!EvmSystem::account_exists(&account_id));
+
+		// Set block number to enable events.
+		System::set_block_number(1);
+
+		// Invoke the function under test.
+		<Account<Test>>::try_mutate_exists(account_id, |maybe_data| -> Result<(), ()> {
+			*maybe_data = None;
+			Ok(())
+		})
+		.unwrap();
+
+		// Assert state changes.
+		assert!(!EvmSystem::account_exists(&account_id));
+	});
+}
+
 /// This test verifies that try_mutate_exists works as expected in case getting error
 /// during data mutation.
 #[test]
@@ -267,33 +293,5 @@ fn try_mutate_exists_fails_without_changes() {
 		// Assert state changes.
 		assert!(EvmSystem::account_exists(&account_id));
 		assert_eq!(EvmSystem::get(&account_id), data);
-	});
-}
-
-/// This test verifies that try_mutate_exists works as expected in case data wasn't providing
-/// and error happens during data mutation.
-#[test]
-fn try_mutate_exists_account_not_created() {
-	new_test_ext().execute_with(|| {
-		// Prepare test data.
-		let account_id = H160::from_str("1000000000000000000000000000000000000001").unwrap();
-
-		// Check test preconditions.
-		assert!(!EvmSystem::account_exists(&account_id));
-
-		// Set block number to enable events.
-		System::set_block_number(1);
-
-		// Invoke the function under test.
-		assert_noop!(
-			<Account<Test>>::try_mutate_exists(account_id, |maybe_data| -> Result<(), ()> {
-				*maybe_data = None;
-				Err(())
-			}),
-			()
-		);
-
-		// Assert state changes.
-		assert!(!EvmSystem::account_exists(&account_id));
 	});
 }
