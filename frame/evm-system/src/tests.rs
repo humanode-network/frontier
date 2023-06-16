4025010496d54697898e1ecd2c400f16d8e81a20
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-06-16 09:19:19 +0300

    Add data changes for try_mutate_exists_fails_without_changes test

--- frame/evm-system/src/tests.rs
+++ frame/evm-system/src/tests.rs
@@ -256,8 +256,11 @@ fn try_mutate_exists_fails_without_changes() {
 		assert!(EvmSystem::account_exists(&account_id));
 
 		// Invoke the function under test.
-		<Account<Test>>::try_mutate_exists(account_id, |_maybe_data| -> Result<(), ()> { Err(()) })
-			.unwrap_err();
+		<Account<Test>>::try_mutate_exists(account_id, |maybe_data| -> Result<(), ()> {
+			*maybe_data = None;
+			Err(())
+		})
+		.unwrap_err();
 
 		// Assert state changes.
 		assert!(EvmSystem::account_exists(&account_id));
