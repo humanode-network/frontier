db82c69e22c5a3c586f682b75f6e5c5e663a37cb
Dmitry Lavrenov <lawrenowdima@gmail.com>
2023-10-12 23:45:14 +0300

    Fix mock

--- frame/evm-system/src/mock.rs
+++ frame/evm-system/src/mock.rs
@@ -17,13 +17,10 @@
 
 //! Test mock for unit tests.
 
-use frame_support::{
-	traits::{ConstU32, ConstU64},
-};
+use frame_support::traits::{ConstU32, ConstU64};
 use mockall::mock;
 use sp_core::{H160, H256};
 use sp_runtime::{
-	generic,
 	traits::{BlakeTwo256, IdentityLookup}, BuildStorage,
 };
 use sp_std::{boxed::Box, prelude::*};
@@ -48,15 +45,10 @@ mock! {
 	}
 }
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 frame_support::construct_runtime! {
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
+	pub enum Test {
 		System: frame_system,
 		EvmSystem: pallet_evm_system,
 	}
@@ -68,13 +60,12 @@ impl frame_system::Config for Test {
 	type BlockLength = ();
 	type RuntimeOrigin = RuntimeOrigin;
 	type RuntimeCall = RuntimeCall;
-	type Index = u64;
-	type BlockNumber = u64;
+	type Nonce = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
 	type AccountId = H160;
 	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = generic::Header<u64, BlakeTwo256>;
+	type Block = Block;
 	type RuntimeEvent = RuntimeEvent;
 	type BlockHashCount = ConstU64<250>;
 	type DbWeight = ();
@@ -102,7 +93,7 @@ impl pallet_evm_system::Config for Test {
 /// Using this call requires manual assertions on the genesis init logic.
 pub fn new_test_ext() -> sp_io::TestExternalities {
     // Build genesis.
-    let config = GenesisConfig {
+    let config = RuntimeGenesisConfig {
         ..Default::default()
     };
     let storage = config.build_storage().unwrap();
@@ -128,7 +119,7 @@ pub trait TestExternalitiesExt {
         E: for<'e> FnOnce(&'e ()) -> R;
 }
 
-impl TestExternalitiesExt for frame_support::sp_io::TestExternalities {
+impl TestExternalitiesExt for sp_io::TestExternalities {
     fn execute_with_ext<R, E>(&mut self, execute: E) -> R
     where
         E: for<'e> FnOnce(&'e ()) -> R,
