58e970d7b677c904bc83bd4653d237ed5ef83e12
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2023-11-22 12:13:45 +0300

    Support external account provider (#83) (#92)
    * Introduce external account provider into `pallet_evm` (#61)
    
    * Introduce AccountProvider interface
    
    * Apply account provider logic for tests and template
    
    * Fix docs for remove_account method
    
    * Fix missing docs
    
    * Improve docs for account provider trait
    
    * Fix docs for native system account provider
    
    * Move account provider logic to separate mod
    
    * Add details docs for methods
    
    * Move `AccountProvider` interface into `fp-em` (#71)
    
    * Move account provider interface into fp-em
    
    * Implement AccountProvider interface for EvmSystem
    
    * Revert formatter
    
    * Move NativeSystemAccountProvider to pallet-evm

--- frame/ethereum/src/mock.rs
+++ frame/ethereum/src/mock.rs
@@ -160,6 +160,7 @@ impl AddressMapping<AccountId32> for HashedAddressMapping {
 }
 
 impl pallet_evm::Config for Test {
+	type AccountProvider = pallet_evm::NativeSystemAccountProvider<Self>;
 	type FeeCalculator = FixedGasPrice;
 	type GasWeightMapping = pallet_evm::FixedGasWeightMapping<Self>;
 	type WeightPerGas = WeightPerGas;
