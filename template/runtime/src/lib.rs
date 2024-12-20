2f8bcde5a66a462b4fb3842d85777fe45438b6e5
Petr Ermishkin <petr@humanode.io>
2024-12-20 19:44:11 +0300

    Tune EVM fee params according to humanode; biomapper tests work

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -312,19 +312,19 @@ impl<F: FindAuthor<u32>> FindAuthor<H160> for FindAuthorTruncated<F> {
 	}
 }
 
-const BLOCK_GAS_LIMIT: u64 = 75_000_000;
-const MAX_POV_SIZE: u64 = 5 * 1024 * 1024;
+const BLOCK_GAS_LIMIT: u64 = 60_000_000;
+// const MAX_POV_SIZE: u64 = 5 * 1024 * 1024;
 
 parameter_types! {
 	pub BlockGasLimit: U256 = U256::from(BLOCK_GAS_LIMIT);
-	pub const GasLimitPovSizeRatio: u64 = BLOCK_GAS_LIMIT.saturating_div(MAX_POV_SIZE);
+	pub const GasLimitPovSizeRatio: u64 = 0;
 	pub PrecompilesValue: FrontierPrecompiles<Runtime> = FrontierPrecompiles::<_>::new();
 	pub WeightPerGas: Weight = Weight::from_parts(weight_per_gas(BLOCK_GAS_LIMIT, NORMAL_DISPATCH_RATIO, WEIGHT_MILLISECS_PER_BLOCK), 0);
 }
 
 /// A simple fixed fee per gas calculator.
 pub struct EvmFeePerGas;
-pub const FEE_PER_GAS: u128 = 1_000_000_000;
+pub const FEE_PER_GAS: u128 = 10_000_000_000_000;
 
 impl fp_evm::FeeCalculator for EvmFeePerGas {
 	fn min_gas_price() -> (U256, Weight) {
