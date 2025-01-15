3d1f8d7ac1aa806c6f95add74a35a1af29672268
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-01-15 21:21:12 +0300

    Add pallet-evm dependency

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -49,7 +49,7 @@ pub mod pallet {
 	pub struct Pallet<T>(PhantomData<T>);
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config: frame_system::Config + pallet_evm::Config {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
