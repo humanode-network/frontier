59fb0812df127a8802574d77449366d84d150300
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-01-16 00:56:01 +0300

    Improve AccountProvider related naming

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -174,8 +174,8 @@ impl<T: Config> Pallet<T> {
 		});
 	}
 
-	/// Create an account having code.
-	pub fn create_code_account(who: &<T as Config>::AccountId) -> AccountCreationOutcome {
+	/// Create a new record related to contract account.
+	pub fn create_contract_account_record(who: &<T as Config>::AccountId) -> AccountCreationOutcome {
 		if Self::account_exists(who) {
 			return AccountCreationOutcome::AlreadyExists;
 		}
@@ -188,8 +188,8 @@ impl<T: Config> Pallet<T> {
 		AccountCreationOutcome::Created
 	}
 
-	/// Remove an account having code.
-	pub fn remove_code_account(who: &<T as Config>::AccountId) -> AccountRemovalOutcome {
+	/// Remove an existed record related to contract account.
+	pub fn remove_contract_account_record(who: &<T as Config>::AccountId) -> AccountRemovalOutcome {
 		if !Self::account_exists(who) {
 			return AccountRemovalOutcome::DidNotExist;
 		}
@@ -256,12 +256,12 @@ impl<T: Config> fp_evm::AccountProvider for Pallet<T> {
 	type AccountId = <T as Config>::AccountId;
 	type Index = <T as Config>::Index;
 
-	fn create_account(who: &Self::AccountId) {
-		let _ = Self::create_code_account(who);
+	fn create_contract_account_record(who: &Self::AccountId) {
+		let _ = Self::create_contract_account_record(who);
 	}
 
-	fn remove_account(who: &Self::AccountId) {
-		let _ = Self::remove_code_account(who);
+	fn remove_contract_account_record(who: &Self::AccountId) {
+		let _ = Self::remove_contract_account_record(who);
 	}
 
 	fn account_nonce(who: &Self::AccountId) -> Self::Index {
