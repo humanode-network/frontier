30b573631b2c36606907b2eda04880fc7f351f36
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-01-17 15:42:51 +0300

    Set has_code to true in case account exists

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -179,6 +179,7 @@ impl<T: Config> Pallet<T> {
 		who: &<T as Config>::AccountId,
 	) -> AccountCreationOutcome {
 		if Self::account_exists(who) {
+			Account::<T>::mutate(who, |account| account.has_code = true);
 			return AccountCreationOutcome::AlreadyExists;
 		}
 
