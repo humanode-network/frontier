37dda6059c1dc28e0048887218739b414b213478
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-01-20 16:14:20 +0300

    Apply fmt

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -178,9 +178,7 @@ impl<T: Config> Pallet<T> {
 	}
 
 	/// Create a new record related to evm managed account.
-	pub fn create_evm_managed_account(
-		who: &<T as Config>::AccountId,
-	) -> AccountCreationOutcome {
+	pub fn create_evm_managed_account(who: &<T as Config>::AccountId) -> AccountCreationOutcome {
 		if Self::account_exists(who) {
 			Account::<T>::mutate(who, |account| account.managed_by_evm = true);
 			return AccountCreationOutcome::AlreadyExists;
@@ -202,7 +200,9 @@ impl<T: Config> Pallet<T> {
 
 		let account_info = Account::<T>::get(who);
 
-		if !account_info.managed_by_evm || account_info.data != <T as Config>::AccountData::default() {
+		if !account_info.managed_by_evm
+			|| account_info.data != <T as Config>::AccountData::default()
+		{
 			return AccountRemovalOutcome::Retained;
 		}
 
