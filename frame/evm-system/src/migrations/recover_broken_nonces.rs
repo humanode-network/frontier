588d2d695315e2b7ba42171ce9414a02ae71a555
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-01-30 17:49:46 +0300

    Remove unused logic at migrations and leave todo for future implementations

--- frame/evm-system/src/migrations/recover_broken_nonces.rs
+++ frame/evm-system/src/migrations/recover_broken_nonces.rs
@@ -1,4 +1,4 @@
-//! Migration to Version 1.
+//! Migration to recover broken nonces.
 
 #[cfg(feature = "try-runtime")]
 use frame_support::sp_std::{vec, vec::Vec};
@@ -12,99 +12,32 @@ pub trait EvmProvider<AccountId> {
 	fn is_managed_by_evm(account_id: &AccountId) -> bool;
 }
 
-/// Execute migration to Version 1 from Version 0.
-pub struct MigrationV0ToV1<EP, T>(sp_std::marker::PhantomData<(EP, T)>);
+/// Execute migration to recover broken nonces.
+pub struct MigrationBrokenNoncesRecover<EP, T>(sp_std::marker::PhantomData<(EP, T)>);
 
 impl<EP: EvmProvider<<T as Config>::AccountId>, T: Config> OnRuntimeUpgrade
-	for MigrationV0ToV1<EP, T>
+	for MigrationBrokenNoncesRecover<EP, T>
 {
 	fn on_runtime_upgrade() -> Weight {
-		let onchain_version = Pallet::<T>::on_chain_storage_version();
-		let pallet_name = Pallet::<T>::name();
+		info!("{}: Running migration to recover broken nonces", pallet_name);
 
-		let mut weight: Weight = T::DbWeight::get().reads(1);
+		// TODO: implement a logic to recover broken nonces.
+		todo!()
 
-		if onchain_version != 0 {
-			info!(
-				"{}: Not at version 0, nothing to do. This migrarion probably should be removed",
-				pallet_name,
-			);
-			return weight;
-		}
-
-		info!("{}: Running migration to v1", pallet_name);
-
-		<Account<T>>::translate::<AccountInfoV0<<T as Config>::Index, <T as Config>::AccountData>, _>(
-			|account_id, old_account_info| {
-				let managed_by_evm = EP::is_managed_by_evm(&account_id);
-				let account_info = AccountInfo::<_, _> {
-					nonce: old_account_info.nonce,
-					managed_by_evm,
-					data: old_account_info.data,
-				};
-
-				weight.saturating_accrue(T::DbWeight::get().reads_writes(1, 1));
-				Some(account_info)
-			},
-		);
-
-		// Set storage version to `1`.
-		StorageVersion::new(1).put::<Pallet<T>>();
-		weight.saturating_accrue(T::DbWeight::get().writes(1));
-
-		info!("{}: Migrated to v1", pallet_name);
+		info!("{}: Migrated", pallet_name);
 
 		weight
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-		/// The Version 0 account storage.
-		#[storage_alias]
-		type Account<T: Config> = StorageMap<
-			Pallet<T>,
-			Blake2_128Concat,
-			<T as Config>::AccountId,
-			AccountInfoV0<<T as Config>::Index, <T as Config>::AccountData>,
-			ValueQuery,
-		>;
-
-		let onchain = <Pallet<T>>::on_chain_storage_version();
-
-		// Disable the check for newer versions by returning an empty state.
-		if onchain >= 1 {
-			return Ok(vec![]);
-		}
-
-		let pre_count: u64 = <Account<T>>::iter().count().try_into().unwrap();
-
-		Ok(pre_count.encode())
+		// TODO: some checks before migration.
+		todo!()
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(state: Vec<u8>) -> Result<(), &'static str> {
-		// Empty state means that the check is disabled.
-		if state.is_empty() {
-			return Ok(());
-		}
-
-		// Ensure version is updated correctly.
-		let onchain = <Pallet<T>>::on_chain_storage_version();
-		assert_eq!(onchain, 1);
-
-		// Ensure the accounts count matches.
-		let pre_count: u64 = scale_codec::Decode::decode(&mut &*state).unwrap();
-		let post_count: u64 = Account::<T>::iter().count().try_into().unwrap();
-		assert_eq!(pre_count, post_count);
-
-		// Ensure storage data is updated correctly.
-		Account::<T>::iter().for_each(|(_account_id, account_info)| {
-			assert!(
-				account_info.managed_by_evm == true || account_info.managed_by_evm == false,
-				"none of accounts should be in destroying status, or undefined state"
-			)
-		});
-
-		Ok(())
+		// TODO: some checks after migration.
+		todo!()
 	}
 }
