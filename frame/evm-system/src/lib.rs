4366fb23597608d1b97d1c3ebc1a7819716fd71f
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-03 00:26:18 +0300

    Do the check inside mutate

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -160,16 +160,14 @@ impl<T: Config> Pallet<T> {
 
 	/// Increment a particular account's nonce by 1.
 	pub fn inc_account_nonce(who: &<T as Config>::AccountId) {
-		let was_existed = Self::account_exists(who);
-
 		Account::<T>::mutate(who, |a| {
 			a.nonce += <T as Config>::Index::one();
-		});
 
-		// Meaning that account is being created.
-		if !was_existed {
-			Self::on_created_account(who.clone());
-		}
+			// Meaning that account is being created.
+			if !Self::account_exists(who) {
+				Self::on_created_account(who.clone());
+			}
+		});
 	}
 
 	/// Create an account.
