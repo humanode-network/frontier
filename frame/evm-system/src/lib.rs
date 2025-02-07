b7c0116387a25cf252da506c9e30db569d45e3d2
Dmitry Lavrenov <lawrenowdima@gmail.com>
2025-02-08 01:25:11 +0300

    Avoid using get_or_insert_default

--- frame/evm-system/src/lib.rs
+++ frame/evm-system/src/lib.rs
@@ -147,8 +147,10 @@ impl<T: Config> Pallet<T> {
 		let is_new_account = Account::<T>::mutate_exists(who, |maybe_account| {
 			let is_new_account = maybe_account.is_none();
 
-			let account = maybe_account.get_or_insert_default();
+			let mut account = maybe_account.take().unwrap_or_default();
 			account.nonce += <T as Config>::Index::one();
+
+			*maybe_account = Some(account);
 
 			is_new_account
 		});
